1e2632807de5ba7d3449a6f962a8a58a3d858a28
Sadra Shahab <sadra.m.shahab@gmail.com>
2014-11-21 15:45:14 -0500

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -67,11 +67,12 @@ define({
     "search": true,
     //Add the home extent button to the toolbar
     "home": true,
+    "description": "Here is my description",
     //Add the geolocation button on the toolbar. Only displayed if browser supports geolocation
     "locate": true,
     //Specify a title for the application. If not provided the web map title is used.
     "title": "Neighborhood Data Portal",
-    "details": "blah blah blah blah",
+    
     //This option demonstrates how to handle additional custom url parameters. For example
     //if you want users to be able to specify lat/lon coordinates that define the map's center or
     //specify an alternate basemap via a url parameter.
