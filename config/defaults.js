341cdba07bb2828e892f8b9d776c097961512022
Sadra Shahab <sadra.m.shahab@gmail.com>
2014-12-12 18:25:33 -0500

    Adding link to the first line

--- config/defaults.js
+++ config/defaults.js
@@ -68,7 +68,7 @@ define({
     //Add the home extent button to the toolbar
     "home": true,
     // value for the "details" button in the app 
-   "description": "<b><p>List of Data Sources</p></b> <p>here is the second thing</p>",
+   "description": "<b><p>List of Data Sources</p></b> <p> <a href="http://www.nyc.gov/html/dcp/pdf/bytes/nynta_metadata.pdf"> Neighborhood Tabulation Area: New York City Department of City Planning</a></p>",
     //Add the geolocation button on the toolbar. Only displayed if browser supports geolocation
     "locate": true,
     //Specify a title for the application. If not provided the web map title is used.
